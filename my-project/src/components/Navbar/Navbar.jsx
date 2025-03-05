066da189990bb49e59a187a93cb4604d59074aa7
spriveen <rpeanuka@gmail.com>
2025-03-04 20:00:40 -0800

    update ResposiveNavbar.jsx

--- my-project/src/components/Navbar/Navbar.jsx
+++ my-project/src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
-import React from 'react'
-import { FaLeaf, FaShoppingCart } from 'react-icons/fa'
-import { MdMenu } from 'react-icons/md'
+import React from 'react';
+import { FaLeaf, FaShoppingCart } from 'react-icons/fa';
+import { MdMenu } from 'react-icons/md';
+import ResponsiveMenu from './ResponsiveMenu';
 
 const NavbarMenu = [
     { id: 1, title: 'Home', link: '/' },
@@ -11,41 +12,53 @@ const NavbarMenu = [
 ];
 
 const Navbar = () => {
-    return (
-        <nav>
-            <div className='container flex justify-between items-center py-4 md:pt-4'> 
-                {/* Logo Section */}
-                <div className='text-2xl flex items-center gap-2 font-bold uppercase'>
-                    <p className='text-red-300'>Fruit</p> 
-                    <p className='text-red-400'>Store</p>
-                    <FaLeaf className='text-green-500'/>
-                </div>
-                
-                {/* Menu Section */}
-                <div className='hidden md:block'>
-                    <ul className='flex items-center gap-6 text-gray-700'>
-                        {NavbarMenu.map((menu) => (
-                            <li key={menu.id}>
-                                <a href={menu.link}
-                                    className='inline-block py-1 px-3 hover:text-red-300 hover:shadow-[0_3px_0_-1px_#ef4444] font-semibold'
-                                >
-                                    {menu.title}
-                                </a>
-                            </li>
-                        ))}
-                        <button>
-                            <FaShoppingCart className='text-2xl hover:bg-red-800 hover:text-white rounded-2xl p-2 duration-200'/>
-                        </button>
-                    </ul>
-                </div>
+    const [open, setOpen] = React.useState(false);
 
-                {/* Mobile Hamburger Menu Section */}
-                <div className='md:hidden'>
-                    <MdMenu className="text-4xl"/> 
+    return (
+        <>
+            <nav>
+                <div className='container flex justify-between items-center py-4 md:pt-4'>
+                    
+                    {/* Logo Section */}
+                    <div className='text-2xl flex items-center gap-2 font-bold uppercase'>
+                        <p className='text-red-300'>Fruit</p> 
+                        <p className='text-red-400'>Store</p>
+                        <FaLeaf className='text-green-500' />
+                    </div>
+
+                    {/* Menu Section */}
+                    <div className='hidden md:block'>
+                        <ul className='flex items-center gap-6 text-gray-700'>
+                            {NavbarMenu.map((menu) => (
+                                <li key={menu.id}>
+                                    <a href={menu.link}
+                                        className='inline-block py-1 px-3 hover:text-red-300 hover:shadow-[0_3px_0_-1px_#dc2626] font-semibold'
+                                    >
+                                        {menu.title}
+                                    </a>
+                                </li>
+                            ))}
+                            <button className='text-2xl hover:bg-red-400 text-white rounded-full p-2 duration-200'>
+                                <FaShoppingCart className='text-2xl' />
+                            </button>
+                        </ul>
+                    </div>
+
+                    {/* Mobile Hamburger Menu Section */}
+                    <div className='md:hidden' onClick={()=>
+                        setOpen(!open)
+                    }>
+                        <button onClick={() => setOpen(!open)}>
+                            <MdMenu className="text-4xl" />
+                        </button>
+                    </div>
                 </div>
-            </div>
-        </nav>
-    )
+            </nav>
+
+            {/* Responsive Menu Section */}
+            <ResponsiveMenu open={open} setOpen={setOpen} />
+        </>
+    );
 }
 
 export default Navbar;
--- my-project/src/components/Navbar/ResponsiveMenu.jsx
+++ my-project/src/components/Navbar/ResponsiveMenu.jsx
@@ -1,8 +1,31 @@
 import React from 'react'
+import {motion , AnimatePresence} from 'framer-motion'
 
-const ResponsiveMenu = () => {
+const ResponsiveMenu = ({open}) => {
   return (
-    <div>ResponsiveMenu</div>
+  <AnimatePresence mode='wait'>
+   {
+    open &&(
+      <motion.div 
+        initial={{opacity:0, y: -100}}
+        animate={{opacity:1, y:0}}
+        exit={{opacity:0, y: -100}}
+        transition={{duration:0.3}}
+      className='absolute top-20 left-0 w-full h-screen z-20'>
+        <div className='text-xl font-semibold uppercase bg-red-500
+        text-white py-10 m-6 rounded-3xl'>
+          <ul className='flex fex-col items-center gap-10'>
+            <li>Home</li>
+            <li>About</li>
+            <li>Service</li>
+            <li>Contact</li>
+
+          </ul>
+        </div>
+      </motion.div>
+    )
+   }
+  </AnimatePresence>  
   )
 }
 
